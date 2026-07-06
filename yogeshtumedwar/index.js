a8adafceed3b7f185ab46b70aff0a253cc0e0113
Yogesh Tumedwar <yogitumedwar2005@gmail.com>
2026-07-06 12:33:09 +0530

    web-assignment

---
+++ yogeshtumedwar/index.js
@@ -0,0 +1,16 @@
+function add() {
+    // debugger;
+    // let ans1 = document.getElementById("output").innerHTML;
+
+    let num;
+    let ans;
+
+    num = 10;
+    ans = num + num;
+document.getElementById("output").inner.html='ans'
+    debugger;
+
+    console.log(ans);
+}
+
+add();
