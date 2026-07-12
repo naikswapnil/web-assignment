64aefa23a8eaec1aa304bced0106cd2b8797baf3
Swapnil Naik <nswapnil97@gmail.com>
2026-07-12 16:59:49 +0530

    Complete Front end concepts covered in classes,notes and assignments

---
+++ Frontend/CSS/index.js
@@ -0,0 +1,16 @@
+function add(){
+    debugger;
+    //  debugger; 
+    // let ans1=document.getElementById.innerHTML("output");
+ var ans1;
+ ans1 = 30;
+ let num;
+ let ans;
+ num=10;
+ ans=num+num;
+document.getElementById("output").innerHTML = ans;
+ console.log(ans);
+ console.log(ans1);
+}
+
+add();
